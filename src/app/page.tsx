f8d1702e0c384520ecb157e1fcfb694dcc6184b1
caffeineeee <cevinsam11@gmail.com>
2024-02-02 20:04:14 +0700

    Add button, input, nav-menu, icons from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			p
+			<div className="flex w-full max-w-sm items-center space-x-2">
+				<Input type="url" placeholder="https://..." />
+				<Button type="submit">Shorten Link</Button>
+			</div>
 		</main>
 	);
 }
